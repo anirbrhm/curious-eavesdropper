d6d1bd8cdf61c17ea2a0885717444756c71ec432
Anirudha Brahma <anirbrhm06@gmail.com>
2022-03-26 03:09:49 +0530

    Added Important Functions

--- cpp-seq-codes/fns.cpp
+++ cpp-seq-codes/fns.cpp
@@ -1,7 +1,44 @@
+#include <iostream> 
 #include "spins.cpp" 
 #include "para.h"
+#include <cstdlib>
+#include <math.h>
+
+using namespace std ; 
+
+int mod(int a){
+    if(a >= 0) return a ; 
+    else return -a ; 
+}
 
 void advance(Spins &S){
     int Enow = S.energy() ; 
-    
+    E[0] = Enow ; 
+    m[0] = S.mag() ; 
+
+    int step = 0 ; 
+
+    while ((mod(m[step]) < eps) || ( (1 - mod(m[step]))< eps  ) || (step < max_steps)){
+        step += 1 ; 
+        int i = rand()%(N+1) + 1 ;
+        int j = rand()%(M+1) + 1 ; 
+
+        S.neg(i,j) ; 
+        long int Enext = S.energy() ; 
+
+        cout << "step : " << step << ", Enow : " << Enow << ", Enext : " << Enext << ", <m> = " << S.mag() << endl ; 
+        float dE = Enext - Enow ; 
+
+        if ((float)(rand()%10)/10 < exp(-dE/T)){
+            Enow = Enext ; 
+        }
+        else{
+            S.neg(i,j) ; 
+        }
+
+        E[step] = Enow ; 
+        m[step] = S.mag() ; 
+    } 
+
+    cout << "Avg mag = " << S.mag() ; 
 }

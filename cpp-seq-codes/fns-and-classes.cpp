6b7a91e87b0121ba0a77e9e788acee9c4a3c0850
Anirudha Brahma <anirbrhm06@gmail.com>
2022-03-31 02:34:15 +0530

    Fixed Bugs to make serial code fully functional

--- cpp-seq-codes/fns-and-classes.cpp
+++ cpp-seq-codes/fns-and-classes.cpp
@@ -40,12 +40,12 @@ class Spins{
         }        
 
         for(int i=0 ; i<A[0].size() ; i++){
-            for(int j=0 ; j<A.size() ; j++){
+            for(int j=0 ; j<A.size()-1 ; j++){
                 res += A[j][i] * A[j+1][i] ; 
             }
         }
         
-        return res ; 
+        return res/2 ; 
     }
 
     void neg(int i, int j){

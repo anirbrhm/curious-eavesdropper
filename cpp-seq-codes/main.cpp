48b3852d0dd9252d2d62d132b3e9f934ed5f097f
Anirudha Brahma <anirbrhm06@gmail.com>
2022-04-09 04:22:37 +0530

    Added Cpp Plots
    Added plots to print magnetization wrt number of iterations and Initial and Final Matrices

--- cpp-seq-codes/main.cpp
+++ cpp-seq-codes/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "fns-and-classes.cpp" 
 #include <chrono>
+#include <fstream>
 
 using namespace std ; 
 
@@ -9,12 +10,29 @@ int main(){
     Spins S = Spins() ; 
     cout << S.energy() << endl ;
 
+    ofstream outAinit("Ainit.txt") ;
+    for(int i=0 ; i<S.A.size() ; i++){
+        for(int j=0 ; j<S.A[i].size() ; j++){
+            outAinit << S.A[i][j] << "\n" ; 
+        }
+    }
+
     advance(S) ;  
 
     auto stop = chrono::high_resolution_clock::now() ;
     auto duration = chrono::duration_cast<chrono::microseconds>(stop - start) ;
 
     cout << "The taken for the program : " << duration.count()/1000000 << " seconds" << endl ;
+
+    ofstream outm("m.txt");
+    for(int i=0 ; i<m.size() ; i++) outm << m[i] << "\n" ; 
+
+    ofstream outa("A.txt") ; 
+    for(int i=0 ; i<S.A.size() ; i++){
+        for(int j=0 ; j<S.A[i].size() ; j++){
+            outa << S.A[i][j] << "\n" ; 
+        }
+    }
 
     return 0 ; 
 }

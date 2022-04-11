a0df5e9db796553e5260fefe80806fbaa107e4f4
Anirudha Brahma <anirbrhm06@gmail.com>
2022-04-12 02:51:20 +0530

    Improved Parallel Codes

--- cpp-parallel-codes/parallel.cpp
+++ cpp-parallel-codes/parallel.cpp
@@ -1,120 +1,12 @@
 #include <mpi.h>
-#include <iostream> 
-#include <algorithm>
+#include <iostream>
+#include "fns-and-classes.cpp" 
 #include <chrono>
-#include <vector>
-#include <math.h>
-#include <cstdlib>
-#include <ctime>
+#include <fstream>
 
 using namespace std ; 
 
-int N = 100 ; 
-int M = 100 ;  
-int max_steps = 50000 ; 
-
-std::vector<int> E(max_steps + 1, 0) ; 
-std::vector<float> m(max_steps + 1, 0) ;
-
-float T = 10 ; 
-float eps = 0.01 ; 
-
-float mod(float a){
-    if(a >= 0) return a ; 
-    else return -a ; 
-}
-
-class Spins{
-  public : 
-    vector<vector<int>> A ;
-
-    Spins(){
-        A = vector<vector<int>>(N+2, vector<int>(M+2, 1)) ;  
-
-        srand((unsigned) time(0));
-
-        for(int i=0 ; i<A.size() ; i++){
-            for(int j=0 ; j<A[0].size() ; j++){
-                if(i == A.size()-1 || i == 0 || j == A[0].size()-1 || j == 0) A[i][j] = 0 ; 
-                else{
-                    int random = rand()%10 + 1 ;  
-                    if(random > 7) A[i][j] = -1 ;
-                } 
-            }
-        }
-    }
-    
-    int energy(){
-        int res = 0 ; 
-        for(int i=0 ; i<A.size() ; i++){
-            for(int j=0 ; j<A[0].size()-1 ; j++){
-                res += A[i][j] * A[i][j+1] ; 
-            }
-        }        
-
-        for(int i=0 ; i<A[0].size() ; i++){
-            for(int j=0 ; j<A.size()-1 ; j++){
-                res += A[j][i] * A[j+1][i] ; 
-            }
-        }
-        
-        return res/2 ; 
-    }
-
-    void neg(int i, int j){
-        this->A[i][j] = -this->A[i][j] ; 
-
-        return ; 
-    }
-
-    float mag(){
-        float sum = 0 ; 
-    
-        for(int i=0 ; i<A.size() ; i++){
-            for(int j=0 ; j<A[0].size() ; j++){
-                sum += A[i][j] ; 
-            }
-        }
-
-        return (sum/N)/M ; 
-    }
-
-} ;
-
-void advance(Spins &S){
-    int Enow = S.energy() ; 
-    E[0] = Enow ; 
-    m[0] = S.mag() ; 
-
-    int step = 0 ; 
-
-    while (step < max_steps){
-        step += 1 ; 
-        int i = rand()%(N) + 1 ;
-        int j = rand()%(M) + 1 ; 
-
-        S.neg(i,j) ; 
-        int Enext = S.energy() ; 
-
-        cout << "step : " << step << ", Enow : " << Enow << ", Enext : " << Enext << ", <m> = " << S.mag() << endl ; 
-        float dE = Enext - Enow ; 
-
-        if(dE < 0 || rand()%10 < 10*exp(-dE/T)){
-            Enow = Enext ; 
-        }
-        else{
-            S.neg(i,j) ; 
-        }
-
-        E[step] = Enow ; 
-        m[step] = S.mag() ; 
-    } 
-
-    cout << "Avg mag = " << S.mag() << endl ; 
-}
-
 Spins S = Spins() ; 
-
 auto start = chrono::high_resolution_clock::now() ;
 
 int main(int argc, char *argv[]) {
@@ -124,36 +16,38 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);  
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 
-    if (myid == 0) { // Do the serial part on a single MPI thread
-        printf("Performing serial computation on cpu %d\n", myid);
+    cout << "Hi this is thread " << myid << endl ; 
 
-        int res = 0 ; 
+    if(myid == 0){
+        ofstream outAinit("Ainit.txt") ;
         for(int i=0 ; i<S.A.size() ; i++){
-            for(int j=0 ; j<S.A[0].size()-1 ; j++){
-                res += S.A[i][j] * S.A[i][j+1] ; 
-            }
-        }        
-
-        for(int i=0 ; i<S.A[0].size() ; i++){
-            for(int j=0 ; j<S.A.size()-1 ; j++){
-                res += S.A[j][i] * S.A[j+1][i] ; 
+            for(int j=0 ; j<S.A[i].size() ; j++){
+                outAinit << S.A[i][j] << "\n" ; 
             }
         }
-
-        cout << res/2 << endl ;
     }
 
     advance(S);  // Every MPI thread will run the parallel work
 
+    MPI_Finalize(); 
 
-    if (myid == 0) { // Do the final serial part on a single MPI thread
+    if(myid == 0){
         printf("Performing the final serial computation on cpu %d\n", myid);
         auto stop = chrono::high_resolution_clock::now() ;
         auto duration = chrono::duration_cast<chrono::microseconds>(stop - start) ;
 
         cout << "The taken for the program : " << duration.count()/1000000 << " seconds" << endl ;
-    }
 
-    MPI_Finalize();  
+        ofstream outm("m.txt");
+        for(int i=0 ; i<m.size() ; i++) outm << m[i] << "\n" ; 
+
+        ofstream outa("A.txt") ; 
+        for(int i=0 ; i<S.A.size() ; i++){
+            for(int j=0 ; j<S.A[i].size() ; j++){
+                outa << S.A[i][j] << "\n" ; 
+            }
+        }
+    }
+ 
     return 0;  
 }  
